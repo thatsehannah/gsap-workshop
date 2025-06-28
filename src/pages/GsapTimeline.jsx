8287518ae435c5fdbd2a240d858f3b7f56dfeedc
E. Hannah <echannah631@gmail.com>
2025-06-27 22:18:24 -0400

    implementing gsap timeline

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -1,16 +1,57 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTimeline = () => {
   // TODO: Implement the gsap timeline
+  const timeline = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 1,
+  });
+
+  useGSAP(() => {
+    timeline.to("#yellow-box", {
+      y: 250,
+      rotation: 360,
+      borderRadius: "100%",
+      duration: 2,
+      ease: "back.inOut",
+    });
+    timeline.to("#yellow-box", {
+      x: 250,
+      scale: 2,
+      rotation: 360,
+      borderRadius: "100%",
+      duration: 2,
+      ease: "back.inOut",
+    });
+    timeline.to("#yellow-box", {
+      y: 0,
+      scale: 1,
+      rotation: 360,
+      borderRadius: "100%",
+      duration: 2,
+      ease: "back.inOut",
+    });
+    timeline.to("#yellow-box", {
+      x: 0,
+      scale: 1,
+      rotation: 360,
+      borderRadius: 8,
+      duration: 2,
+      ease: "back.inOut",
+    });
+  }, []);
 
   return (
     <main>
       <h1>GsapTimeline</h1>
 
-      <p className="mt-5 text-gray-500">
+      <p className='mt-5 text-gray-500'>
         The <code>gsap.timeline()</code> method is used to create a timeline
         instance that can be used to manage multiple animations.
       </p>
 
-      <p className="mt-5 text-gray-500">
+      <p className='mt-5 text-gray-500'>
         The <code>gsap.timeline()</code> method is similar to the{" "}
         <code>gsap.to()</code>, <code>gsap.from()</code>, and{" "}
         <code>gsap.fromTo()</code> methods, but the difference is that the{" "}
@@ -22,22 +63,25 @@ const GsapTimeline = () => {
         state, and from a new state to a new state, respectively.
       </p>
 
-      <p className="mt-5 text-gray-500">
+      <p className='mt-5 text-gray-500'>
         Read more about the{" "}
         <a
-          href="https://greensock.com/docs/v3/GSAP/gsap.timeline()"
-          target="_blank"
-          rel="noreferrer noopener nofollow"
+          href='https://greensock.com/docs/v3/GSAP/gsap.timeline()'
+          target='_blank'
+          rel='noreferrer noopener nofollow'
         >
           gsap.timeline()
         </a>{" "}
         method.
       </p>
 
-      <div className="mt-20 space-y-10">
+      <div className='mt-20 space-y-10'>
         <button onClick={() => {}}>Play/Pause</button>
 
-        <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
+        <div
+          id='yellow-box'
+          className='w-20 h-20 bg-yellow-500 rounded-lg'
+        />
       </div>
     </main>
   );
